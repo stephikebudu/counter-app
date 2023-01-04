d8a949be9a2f746f2530c72ca318c3cabd406408
Stephany Ikebudu <stephikebudu@gmail.com>
2023-01-04 18:57:36 +0800

    finished app

--- counter.js
+++ counter.js
@@ -10,6 +10,18 @@ btns.forEach(function(btn) {
         const styles = e.currentTarget.classList;
         if (styles.contains('decrease')) {
             count--;
+            value.style
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black';
         }
         value.textContent = count;
     });
